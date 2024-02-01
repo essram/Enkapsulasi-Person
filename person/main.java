033f05ef2690075b6b26ddc8ee7096a9e4203fb2
essram <esarama22@gmail.com>
2024-02-01 08:52:01 +0700

    javaa personnn

---
+++ person/main.java
@@ -0,0 +1,8 @@
+package person;
+public class main {
+    public static void main(String[] args) {
+    person mPerson = new person();
+    mPerson.name = "gwe";
+    System.out.println("nama gwejh adalah "+ mPerson.getName());
+}
+}
---
+++ person/person.java
@@ -0,0 +1,22 @@
+package person;
+
+//modiefier
+
+class person {
+    public String name;
+
+public void setName(String name){
+    this.name = name;
+}
+
+public String getName(){
+    return this.name;
+    }
+
+    public person(){
+        System.out.println("Method Constructor");
+    }
+
+}
+
+
